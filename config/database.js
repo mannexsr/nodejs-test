251c992cd948c9e25ce870408200897ad7fe2f62
mannexsr <109422982+mannexsr@users.noreply.github.com>
2023-07-01 23:38:15 +0800

    first commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'inventoryapp.cw0mnwj4pig7.us-east-1.rds.amazonaws.com',
+        'user': 'admin',
+        'password': '102711elijah'
+    },
+	'database': 'nodejs',
+    'users_table': 'users'
+};
